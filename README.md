0580c5896760afa5aac7c7dd043ed857b1d02292
Sam Guard <sg2268@bath.ac.uk>
2023-04-12 15:00:44 +0100

    init, added base code for running the env and code for the base agent

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ReinforcementLearningCW2
+This is the perfect RL agent that can solve any problem.
---
+++ agents.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+
+from gymnasium.spaces import Box
+
+from copy import deepcopy
+
+
+class Agent:
+    """
+    Base class for agents. Please inherit this when creating an agent
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.prev_state = None
+        self.action_taken = None
+
+    def choose_action(self, action_space: Box, state: Box):
+        self.prev_state = deepcopy(state)
+        self.action_taken = deepcopy(action_space.sample())
+        return self.action_taken
+    
+    def update(self, new_state: Box, reward: float, is_terminal: bool, is_trunc: bool):
+        pass
