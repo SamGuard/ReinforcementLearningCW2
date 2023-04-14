1b256ab54b8f4aa333cd78664714234d3f312521
Sam Guard <sg2268@bath.ac.uk>
2023-04-14 17:17:50 +0100

    added value approximator and torch example

--- agents.py
+++ agents.py
@@ -23,3 +23,39 @@ class Agent:
     
     def update(self, new_state: Box, reward: float, is_terminal: bool, is_trunc: bool):
         pass
+
+
+class ValueApproximator(nn.Module):
+    def __init__(self, in_dims, out_dims=1, width=8, depth=5, activation_function=F.relu) -> None:
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.act = activation_function
+
+        self.input_layer = nn.Linear(in_dims, width)
+        self.output_layer = nn.Linear(width, out_dims)
+        for i in range(depth - 2):
+            self.layers.append(nn.Linear(width, width))
+        
+    def forward(self, x: Tensor):
+        # x.shape = [n, in_dims]
+        x = self.act(self.input_layer(x))
+
+        for l in self.layers:
+            x = self.act(l(x))
+    
+        return self.output_layer(l(x))
+
+def example_model():
+    f = ValueApproximator(in_dims=3, out_dims=3)
+    optim = torch.optim.Adam(f.parameters(), lr=1e-3)
+
+    x = torch.rand(size=(128, 3))
+    y = torch.rand(size=(128, 3))
+
+    for i in range(100):
+        pred = f(x)
+        optim.zero_grad()
+        loss = F.mse_loss(pred, y)
+        loss.backward()
+        optim.step()
+        print(loss)
