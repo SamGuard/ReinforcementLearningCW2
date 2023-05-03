64e26214fc4c22a6809b5026633b1bf171246f2f
Joseph Cryer <jcryer1234@gmail.com>
2023-05-03 18:15:32 +0100

    TD3 converging

--- agents.py
+++ agents.py
@@ -39,7 +39,7 @@ class ReplayMemory(object):
         self.reward[self.curr] = reward
         self.terminal[self.curr] = terminal
 
-        self.curr = (self.curr + 1) & MAX_MEMORY
+        self.curr = (self.curr + 1) % MAX_MEMORY
         self.size = min(self.size + 1, MAX_MEMORY)
     
     def sample(self, batch_size):
@@ -52,26 +52,6 @@ class ReplayMemory(object):
             torch.tensor(self.reward[rand_elems], dtype=torch.float32),
             torch.tensor(self.terminal[rand_elems], dtype=torch.float32)
         )
-        
-"""
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        #Save a transition
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-"""
-
-"""
------------------------------------------------------------------------------------------
-"""
 
 class Agent:
     """
@@ -93,37 +73,34 @@ class Agent:
     ):
         pass
 
+temp = 16
 class Actor(nn.Module):
     def __init__(self, in_dim, out_dim, max_val):
         super(Actor, self).__init__()
 
-        self.l1 = nn.Linear(in_dim, 128)
-        self.l2 = nn.Linear(128, 128)
-        self.l3 = nn.Linear(128, 128)
-        self.l4 = nn.Linear(128, out_dim)
+        self.l1 = nn.Linear(in_dim, temp)
+        self.l2 = nn.Linear(temp, temp)
+        self.l3 = nn.Linear(temp, out_dim)
         self.max_val = max_val
     
     def forward(self, state):
         out = F.relu(self.l1(state))
         out = F.relu(self.l2(out))
-        out = F.relu(self.l3(out))
-        return self.max_val * torch.tanh(self.l4(out))
+        return self.max_val * torch.tanh(self.l3(out))
     
 class Critic(nn.Module):
     def __init__(self, in_dim, out_dim):
         super(Critic, self).__init__()
         
         # critic 1
-        self.l1 = nn.Linear(in_dim + out_dim, 128)
-        self.l2 = nn.Linear(128, 128)
-        self.l3 = nn.Linear(128, 128)
-        self.l4 = nn.Linear(128, out_dim)
+        self.l1 = nn.Linear(in_dim + out_dim, temp)
+        self.l2 = nn.Linear(temp, temp)
+        self.l3 = nn.Linear(temp, out_dim)
 
         # critic 2
-        self.l5 = nn.Linear(in_dim + out_dim, 128)
-        self.l6 = nn.Linear(128, 128)
-        self.l7 = nn.Linear(128, 128)
-        self.l8 = nn.Linear(128, out_dim)
+        self.l4 = nn.Linear(in_dim + out_dim, temp)
+        self.l5 = nn.Linear(temp, temp)
+        self.l6 = nn.Linear(temp, out_dim)
 
     def forward(self, state, action):
         return self.critic_1(state, action), self.critic_2(state, action)
@@ -132,31 +109,29 @@ class Critic(nn.Module):
         state_action = torch.cat([state, action], 1)
         out = F.relu(self.l1(state_action))
         out = F.relu(self.l2(out))
-        out = F.relu(self.l3(out))
-        out = self.l4(out)
+        out = self.l3(out)
         return out
     
     def critic_2(self, state, action):
         state_action = torch.cat([state, action], 1)
-        out = F.relu(self.l5(state_action))
-        out = F.relu(self.l6(out))
-        out = F.relu(self.l7(out))
-        out = self.l8(out)
+        out = F.relu(self.l4(state_action))
+        out = F.relu(self.l5(out))
+        out = self.l6(out)
         return out
 
 class TD3(Agent):
-    def __init__(self, in_dim, out_dim, max_val, gamma = 0.99, target_learn=0.001, batch_size=100):
+    def __init__(self, in_dim, out_dim, max_val, gamma = 0.99, target_learn=0.01, batch_size=128):
         super().__init__()
         
         self.memory = ReplayMemory(in_dim, out_dim)
 
         self.actor = Actor(in_dim, out_dim, max_val)
         self.actor_target = copy.deepcopy(self.actor)
-        self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+        self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=0.0001)
 
         self.critic = Critic(in_dim, out_dim)
         self.critic_target = copy.deepcopy(self.critic)
-        self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=3e-3)
+        self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=0.001)
 
         self.gamma = gamma
         self.target_learn = target_learn # tau
@@ -164,14 +139,13 @@ class TD3(Agent):
         self.max_val = max_val
 
         self.current_step = 0
+        self.in_dim = in_dim
+        self.out_dim = out_dim
 
 
     def choose_action(self, state, action_space):
         self.prev_state = torch.tensor(state, dtype=torch.float32)
-        if (self.memory.size < 10000):
-            self.action_taken = torch.tensor(action_space.sample(), dtype=torch.float32)
-        else:
-            self.action_taken = self.actor(self.prev_state).detach()
+        self.action_taken = self.actor(self.prev_state).detach()
         return self.action_taken
     
     def update(self, new_state: np.ndarray, reward: float, is_terminal: bool, is_trunc: bool):
@@ -184,18 +158,17 @@ class TD3(Agent):
 
         if self.memory.size < self.batch_size:
             return
-        
-        state, action, next_state, reward, terminal = self.memory.sample(self.batch_size)
-
 
         with torch.no_grad():
-            noise = (torch.randn_like(action) * 0.2).clamp(-0.5, 0.5)
+            state, action, next_state, reward, terminal = self.memory.sample(self.batch_size)
+
+            noise = (torch.rand_like(action) * 0.1).clamp(-0.5, 0.5)
+
             next_action = (self.actor_target(next_state) + noise).clamp(-self.max_val, self.max_val)
             
             target_critic_1, target_critic_2 = self.critic_target(next_state, next_action)
             target_critic = torch.min(target_critic_1, target_critic_2)
-            target_critic = reward + terminal * self.gamma * target_critic
-
+            target_critic = reward + (1-terminal) * self.gamma * target_critic
 
         current_critic_1, current_critic_2 = self.critic(state, action)
         loss_critic = F.mse_loss(current_critic_1, target_critic) + F.mse_loss(current_critic_2, target_critic)
@@ -204,7 +177,7 @@ class TD3(Agent):
         loss_critic.backward()
         self.critic_optim.step()
        
-        if self.current_step % 200 == 0:
+        if self.current_step % 2 == 0:
             loss_actor = -self.critic.critic_1(state, self.actor(state)).mean()
             self.actor_optim.zero_grad()
             loss_actor.backward()
